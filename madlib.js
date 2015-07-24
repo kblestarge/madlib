3efe66db1c75bd5154631122d5d569ddae5a8d24
klestarg <kblestarge@gmail.com>
2015-07-23 21:42:07 -0600

    Still working on getting it functional with Angular. Also cleaning up a lot of garbage code

--- madlib.js
+++ madlib.js
@@ -4,7 +4,30 @@
   app.controller('MainController', function() {
   	var self = this;
   	self.finalWords = {};
-  	self.options = {nouns: '', verbs: '', verbings: '', adverbs: '', adjectives: ''};
+  	self.options = {
+  		nouns: '',
+  		verbs: '',
+  		verbings: '',
+  		adverbs: '',
+  		adjectives: ''
+  	};
+  	self.selectStory = {};
+  	self.asdf = "hello";
+
+  	//object of each story's needed words, then assign to finalWords object
+  	self.preStory = {
+	  	bobby: {nouns: 3, verbs: 4, verbings: 2, adverbs: 5, adjectives: 3},
+	  	techdeck: {nouns: 2, verbs: 4, verbings: 5, adverbs: 5, adjectives: 3},
+	  	ninja: {nouns: 5, verbs: 4, verbings: 2, adverbs: 2, adjectives: 3}
+  	};
+
+  	self.defaults = {
+  		nouns: ["toy", "gun", "church", "Canada", "George's grandfather", "biscuit"], 
+  		verbs: ["run", "fly", "bounce", "kick", "smell"], 
+  		verbings: ["hinking", "flying", "busting", "chopping", "yelling"], 
+  		adverbs: ["extremely", "jankely", "funnily", "grotesquely", "happily"], 
+  		adjectives: ["stanky", "crazy", "sick", "cool", "ugly"]
+  	};
 
   	self.yomama = function(){
   		console.log('hello world!');
@@ -15,18 +38,20 @@
 
   		for(var prop in self.options){
 
-  			self.finalWords[prop] = self.options[prop].split(', ');
-  			// console.log('IP:',initialInputs);
-  			randomize(self.finalWords[prop]);
+  			self.finalWords[prop] = {
+  				words: [self.options[prop].split(', ')],
+  				numNeeded: self.story[prop]
+  			};
+
+  			randomize(self.finalWords[prop].words, self.finalWords[prop].numNeeded);
   		}
+  		console.log(self.finalWords);
 
-
-
-  		//randomize
-  		//Out final Array
+  		//Output final Array
   	}
 
   	function randomize(wordArray, wordsNeeded){
+  		console.log('randomize');
 			//make sure there are no duplicates
 		while(wordArray.length < wordsNeeded){
 
@@ -38,137 +63,136 @@
 			nounRandArray.push(nounRand);
 			console.log("nounRandArray",nounRandArray);
 			inputArray.push(noun[nounRand]);
-
 		}
 
   	}
 
-  	function getInput(wordType,wordsNeeded){
+  // 	function getInput(wordType,wordsNeeded){
 
 
-			console.log("Before inputArray", inputArray);
-			var nounRandArray = [];
-			var nounRand;
-			var verbRandArray = [];
-			var verbRand;
-			var adjectiveRandArray = [];
-			var adjectiveRand;
-			var adverbRandArray = [];
-			var adverbRand;
-			var verbingRandArray = [];
-			var verbingRand;
+		// 	console.log("Before inputArray", inputArray);
+		// 	var nounRandArray = [];
+		// 	var nounRand;
+		// 	var verbRandArray = [];
+		// 	var verbRand;
+		// 	var adjectiveRandArray = [];
+		// 	var adjectiveRand;
+		// 	var adverbRandArray = [];
+		// 	var adverbRand;
+		// 	var verbingRandArray = [];
+		// 	var verbingRand;
 
-			//if default values are needed, load the defaults
-			while(inputArray.length < wordsNeeded){
+		// 	//if default values are needed, load the defaults
+		// 	while(inputArray.length < wordsNeeded){
 
-				if(wordType === "noun"){
-					//make sure there are no duplicates
-					do{
-						nounRand = Math.floor(Math.random()*(noun.length));
-					}while(nounRandArray.indexOf(nounRand) !== -1)
-					nounRandArray.push(nounRand);
-					console.log("nounRandArray",nounRandArray);
-					inputArray.push(noun[nounRand]);
+		// 		if(wordType === "noun"){
+		// 			//make sure there are no duplicates
+		// 			do{
+		// 				nounRand = Math.floor(Math.random()*(noun.length));
+		// 			}while(nounRandArray.indexOf(nounRand) !== -1)
+		// 			nounRandArray.push(nounRand);
+		// 			console.log("nounRandArray",nounRandArray);
+		// 			inputArray.push(noun[nounRand]);
 
-				}else if(wordType === "verb"){
-					//make sure there are no duplicates
-					do{
-						verbRand = Math.floor(Math.random()*(verb.length));
-					}while(verbRandArray.indexOf(verbRand) !== -1)
-					verbRandArray.push(verbRand);
-					console.log("verbRandArray",verbRandArray);
-					inputArray.push(verb[verbRand]);
+		// 		}else if(wordType === "verb"){
+		// 			//make sure there are no duplicates
+		// 			do{
+		// 				verbRand = Math.floor(Math.random()*(verb.length));
+		// 			}while(verbRandArray.indexOf(verbRand) !== -1)
+		// 			verbRandArray.push(verbRand);
+		// 			console.log("verbRandArray",verbRandArray);
+		// 			inputArray.push(verb[verbRand]);
 
-				}else if(wordType === "adjective"){
-					//make sure there are no duplicates
-					do{
-						adjectiveRand = Math.floor(Math.random()*(adjective.length));
-					}while(adjectiveRandArray.indexOf(adjectiveRand) !== -1)
-					adjectiveRandArray.push(adjectiveRand);
-					console.log("adjectiveRandArray",adjectiveRandArray);
-					inputArray.push(adjective[adjectiveRand]);
+		// 		}else if(wordType === "adjective"){
+		// 			//make sure there are no duplicates
+		// 			do{
+		// 				adjectiveRand = Math.floor(Math.random()*(adjective.length));
+		// 			}while(adjectiveRandArray.indexOf(adjectiveRand) !== -1)
+		// 			adjectiveRandArray.push(adjectiveRand);
+		// 			console.log("adjectiveRandArray",adjectiveRandArray);
+		// 			inputArray.push(adjective[adjectiveRand]);
 
-				}else if(wordType === "adverb"){
-					//make sure there are no duplicates
-					do{
-						adverbRand = Math.floor(Math.random()*(adverb.length));
-					}while(adverbRandArray.indexOf(adverbRand) !== -1)
-					adverbRandArray.push(adverbRand);
-					console.log("adverbRandArray",adverbRandArray);
-					inputArray.push(adverb[adverbRand]);
+		// 		}else if(wordType === "adverb"){
+		// 			//make sure there are no duplicates
+		// 			do{
+		// 				adverbRand = Math.floor(Math.random()*(adverb.length));
+		// 			}while(adverbRandArray.indexOf(adverbRand) !== -1)
+		// 			adverbRandArray.push(adverbRand);
+		// 			console.log("adverbRandArray",adverbRandArray);
+		// 			inputArray.push(adverb[adverbRand]);
 
-				}else if(wordType === "verbing"){
-					//make sure there are no duplicates
-					do{
-						verbingRand = Math.floor(Math.random()*(verbing.length));
-					}while(verbingRandArray.indexOf(verbingRand) !== -1)
-					verbingRandArray.push(verbingRand);
-					console.log("verbingRandArray",verbingRandArray);
-					inputArray.push(verbing[verbingRand]);
+		// 		}else if(wordType === "verbing"){
+		// 			//make sure there are no duplicates
+		// 			do{
+		// 				verbingRand = Math.floor(Math.random()*(verbing.length));
+		// 			}while(verbingRandArray.indexOf(verbingRand) !== -1)
+		// 			verbingRandArray.push(verbingRand);
+		// 			console.log("verbingRandArray",verbingRandArray);
+		// 			inputArray.push(verbing[verbingRand]);
 
-				}
-			}
+		// 		}
+		// 	}
 
-			var finalArray = [];
-			console.log("After inputArray", inputArray);
-			var rando;
-			var randoArray = [];
+		// 	var finalArray = [];
+		// 	console.log("After inputArray", inputArray);
+		// 	var rando;
+		// 	var randoArray = [];
 
-			for(i = 0; i < wordsNeeded; i++) {
+		// 	for(i = 0; i < wordsNeeded; i++) {
 
-				//make sure there are no duplicates in final array of random values
-				do{
-					rando = Math.floor(Math.random()*(wordsNeeded));
-				}while(randoArray.indexOf(rando) !== -1)
-				randoArray.push(rando);
+		// 		//make sure there are no duplicates in final array of random values
+		// 		do{
+		// 			rando = Math.floor(Math.random()*(wordsNeeded));
+		// 		}while(randoArray.indexOf(rando) !== -1)
+		// 		randoArray.push(rando);
 
-				console.log("rando",rando);
-				finalArray[i]=inputArray[rando];
-			}
+		// 		console.log("rando",rando);
+		// 		finalArray[i]=inputArray[rando];
+		// 	}
 			
-			console.log("finalArrays",finalArray);
-			return finalArray;
-		}
+		// 	console.log("finalArrays",finalArray);
+		// 	return finalArray;
+		// }
 
 
-		/////////////////
+		// /////////////////
 
-	    if(document.getElementById('a').checked) {
+	 //    if(document.getElementById('a').checked) {
 
-			finalNouns = getInput("noun",3);
-			finalVerbs = getInput("verb",2);
-			finalAdjectives = getInput("adjective",2);
-			finalAdverbs = getInput("adverb",2);
-			finalVerbings = getInput("verbing",1);
+		// 	finalNouns = getInput("noun",3);
+		// 	finalVerbs = getInput("verb",2);
+		// 	finalAdjectives = getInput("adjective",2);
+		// 	finalAdverbs = getInput("adverb",2);
+		// 	finalVerbings = getInput("verbing",1);
 
-			var story = "One <span class='underline'>" + finalAdjectives[0] + "</span> day, Bobby was <span class='underline'>" + finalVerbings[0] + "</span> around the <span class='underline'>" + finalNouns[0] + "</span>, when <span class='underline'>" + finalAdverbs[0] + "</span>, Bobby saw his aquaintence, <span class='underline'>" + finalNouns[1] + "</span>. \"Hey,\" said Bobby, \"Wanna <span class='underline'>" + finalVerbs[0] + "</span> out of here and <span class='underline'>" + finalVerbs[1] + "</span> that <span class='underline'>" + finalNouns[2] + "</span> over there?\" The two friends <span class='underline'>" + finalAdverbs[1] + "</span> became <span class='underline'>" + finalAdjectives[1] + "</span> friends. The end.";
-			this.story = {title: 'Bobby\'s Story', description: story};
+		// 	var story = "One <span class='underline'>" + finalAdjectives[0] + "</span> day, Bobby was <span class='underline'>" + finalVerbings[0] + "</span> around the <span class='underline'>" + finalNouns[0] + "</span>, when <span class='underline'>" + finalAdverbs[0] + "</span>, Bobby saw his aquaintence, <span class='underline'>" + finalNouns[1] + "</span>. \"Hey,\" said Bobby, \"Wanna <span class='underline'>" + finalVerbs[0] + "</span> out of here and <span class='underline'>" + finalVerbs[1] + "</span> that <span class='underline'>" + finalNouns[2] + "</span> over there?\" The two friends <span class='underline'>" + finalAdverbs[1] + "</span> became <span class='underline'>" + finalAdjectives[1] + "</span> friends. The end.";
+		// 	this.story = {title: 'Bobby\'s Story', description: story};
 
-		}else if(document.getElementById('b').checked) {
+		// }else if(document.getElementById('b').checked) {
 
-			finalNouns = getInput("noun",3);
-			finalVerbs = getInput("verb",3);
-			finalAdjectives = getInput("adjective",3);
-			finalAdverbs = getInput("adverb",1);
-			//finalVerbings = getInput("verbing",0);
+		// 	finalNouns = getInput("noun",3);
+		// 	finalVerbs = getInput("verb",3);
+		// 	finalAdjectives = getInput("adjective",3);
+		// 	finalAdverbs = getInput("adverb",1);
+		// 	//finalVerbings = getInput("verbing",0);
 
-		  	var story = "Teckdecks are <span class='underline'>" + finalAdjectives[0] + "</span>. Everyone in the whole <span class='underline'>" + finalAdjectives[1] + "</span> world should <span class='underline'>" + finalVerbs[0] + "</span> one. Techdecks can <span class='underline'>" + finalVerbs[1] + "</span> over <span class='underline'>" + finalNouns[0] + "</span>s. Their wheels <span class='underline'>" + finalAdverbs[0] + "</span> <span class='underline'>" + finalVerbs[2] + "</span> when you do a trick right. Sometimes it's fun to try finding new <span class='underline'>" + finalAdjectives[2] + "</span> places to skate on, like <span class='underline'>" + finalNouns[1] + "</span>s and <span class='underline'>" + finalNouns[2] + "</span>s. The end.";
-			this.story = {title: 'Techdeck Story', description: story};
+		//   	var story = "Teckdecks are <span class='underline'>" + finalAdjectives[0] + "</span>. Everyone in the whole <span class='underline'>" + finalAdjectives[1] + "</span> world should <span class='underline'>" + finalVerbs[0] + "</span> one. Techdecks can <span class='underline'>" + finalVerbs[1] + "</span> over <span class='underline'>" + finalNouns[0] + "</span>s. Their wheels <span class='underline'>" + finalAdverbs[0] + "</span> <span class='underline'>" + finalVerbs[2] + "</span> when you do a trick right. Sometimes it's fun to try finding new <span class='underline'>" + finalAdjectives[2] + "</span> places to skate on, like <span class='underline'>" + finalNouns[1] + "</span>s and <span class='underline'>" + finalNouns[2] + "</span>s. The end.";
+		// 	this.story = {title: 'Techdeck Story', description: story};
 
-		}else if(document.getElementById('c').checked) {
+		// }else if(document.getElementById('c').checked) {
 		  	
-			finalNouns = getInput("noun",3);
-			finalVerbs = getInput("verb",4);
-			finalAdjectives = getInput("adjective",4);
-			finalAdverbs = getInput("adverb",2);
-			finalVerbings = getInput("verbing",1);
+		// 	finalNouns = getInput("noun",3);
+		// 	finalVerbs = getInput("verb",4);
+		// 	finalAdjectives = getInput("adjective",4);
+		// 	finalAdverbs = getInput("adverb",2);
+		// 	finalVerbings = getInput("verbing",1);
 
-		  	var story = "Once upon a <span class='underline'>" + finalAdjectives[0] + "</span> time, there lived a magical ninja who came from the <span class='underline'>" + finalAdjectives[1] + "</span> land of <span class='underline'>" + finalNouns[0] + "</span>s. He liked to <span class='underline'>" + finalVerbs[0] + "</span> <span class='underline'>" + finalAdverbs[0] + "</span>, and it just so happened that the King of the land was announcing a <span class='underline'>" + finalVerbings[0] + "</span> competition, which he thought was similar enough. And so, the ninja went to <span class='underline'>" + finalNouns[1] + "</span> and asked if he could <span class='underline'>" + finalVerbs[1] + "</span> the competition. \"Sorry,\" said the <span class='underline'>" + finalAdjectives[2] + "</span> manager in charge, \"you have to be able to <span class='underline'>" + finalAdverbs[1] + "</span> <span class='underline'>" + finalVerbs[2] + "</span> at least three miles in order to sign up\". The ninja thought that was a dumb prerequisite, so he didn't sign up, but instead began to <span class='underline'>" + finalVerbs[3] + "</span> <span class='underline'>" + finalAdjectives[3] + "</span> <span class='underline'>" + finalNouns[2] + "</span>s. The end.";
-			this.story = {title: 'Magical Ninja Story', description: story};
-		}
-		else{
-			this.story = {title: 'Hey player, choose a story first', description: '-_-'};
-		}
+		//   	var story = "Once upon a <span class='underline'>" + finalAdjectives[0] + "</span> time, there lived a magical ninja who came from the <span class='underline'>" + finalAdjectives[1] + "</span> land of <span class='underline'>" + finalNouns[0] + "</span>s. He liked to <span class='underline'>" + finalVerbs[0] + "</span> <span class='underline'>" + finalAdverbs[0] + "</span>, and it just so happened that the King of the land was announcing a <span class='underline'>" + finalVerbings[0] + "</span> competition, which he thought was similar enough. And so, the ninja went to <span class='underline'>" + finalNouns[1] + "</span> and asked if he could <span class='underline'>" + finalVerbs[1] + "</span> the competition. \"Sorry,\" said the <span class='underline'>" + finalAdjectives[2] + "</span> manager in charge, \"you have to be able to <span class='underline'>" + finalAdverbs[1] + "</span> <span class='underline'>" + finalVerbs[2] + "</span> at least three miles in order to sign up\". The ninja thought that was a dumb prerequisite, so he didn't sign up, but instead began to <span class='underline'>" + finalVerbs[3] + "</span> <span class='underline'>" + finalAdjectives[3] + "</span> <span class='underline'>" + finalNouns[2] + "</span>s. The end.";
+		// 	this.story = {title: 'Magical Ninja Story', description: story};
+		// }
+		// else{
+		// 	this.story = {title: 'Hey player, choose a story first', description: '-_-'};
+		// }
 
   })
 
@@ -192,10 +216,6 @@
 //Controller that accesses words {{noun}}
 //Controller takes inputs and sends it to a service
 
-var noun = ["toy", "gun", "church", "Canada", "George's grandfather", "biscuit"];
-var verb = ["run", "fly", "bounce", "kick", "smell"];
-var verbing = ["hinking", "flying", "busting", "chopping", "yelling"];
-var adjective = ["stanky", "crazy", "sick", "cool", "ugly"];
-var adverb = ["extremely", "jankely", "funnily", "grotesquely", "happily"];
+
 
 
